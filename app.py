963cf7df4d3f5f8ff94884ead67e119f1fc7c666
Ashutosh Singh <ashutosh.singh@smava.de>
2018-07-02 12:55:41 +0200

    added time

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import requests
 import xml.etree.ElementTree
 import re
-z = 0
+import time
+
+start_time = time.time()
+
 app_list = []
 eureka_url = 'http://213.183.195.222:8761/eureka/apps'
 eureka_xml = requests.get(eureka_url)
@@ -10,7 +13,6 @@ for eureka_app in eureka_list.iter('name'):
     app_name = eureka_app.text
     if app_name != 'MyOwn':
         app_list.append(app_name)
-        z = z + 1
         eureka_app_url = eureka_url + '/' + app_name
         eureka_app_xml = requests.get(eureka_app_url)
         eureka_app_instance_list = xml.etree.ElementTree.fromstring(eureka_app_xml.text)
@@ -25,6 +27,4 @@ for eureka_app in eureka_list.iter('name'):
                 except requests.exceptions.RequestException:
                     eureka_app_instance_statusurl_status_code = '001'
                 print(app_name, instance.text, eureka_app_instance_statusurl, eureka_app_instance_statusurl_status_code)
-
-print(z)
-print(len(app_list))
+print("My program took", time.time() - start_time, " seconds to run")
--- app_urllib.py
+++ app_urllib.py
@@ -1,24 +1,26 @@
 import requests
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
 import xml.etree.ElementTree
 import re
-import http.client
-from datetime import datetime
+import time
+import socket
 
-utc_time = datetime.utcnow()
-print(utc_time)
-
-z = 0
+start_time = time.time()
+timeout = 10
+socket.setdefaulttimeout(timeout)
 app_list = []
 eureka_url = 'http://213.183.195.222:8761/eureka/apps'
 eureka_xml = requests.get(eureka_url)
 eureka_list = xml.etree.ElementTree.fromstring(eureka_xml.text)
+print("############################################", time.time() - start_time)
 for eureka_app in eureka_list.iter('name'):
     app_name = eureka_app.text
     if app_name != 'MyOwn':
         app_list.append(app_name)
-        z = z + 1
         eureka_app_url = eureka_url + '/' + app_name
         eureka_app_xml = requests.get(eureka_app_url)
+        print("...........................................", time.time() - start_time)
         eureka_app_instance_list = xml.etree.ElementTree.fromstring(eureka_app_xml.text)
         for instance in eureka_app_instance_list.iter('instanceId'):
             if re.search(':',instance.text):
@@ -27,17 +29,12 @@ for eureka_app in eureka_list.iter('name'):
                 eureka_app_instance_text = xml.etree.ElementTree.fromstring(eureka_app_instance_xml.text)
                 eureka_app_instance_statusurl = eureka_app_instance_text.find('statusPageUrl').text
                 try:
-                    eureka_app_instance_statusurl_conn = http.client.HTTPConnection(eureka_app_instance_statusurl)
-                    eureka_app_instance_statusurl_conn.request("GET", "/")
-                    eureka_app_instance_statusurl_conn_response = eureka_app_instance_statusurl_conn.getresponse()
-                    eureka_app_instance_statusurl_status_code = eureka_app_instance_statusurl_conn_response.status
-                    eureka_app_instance_statusurl_conn.close()
-                except requests.exceptions.RequestException:
+                    #eureka_app_instance_statusurl_request = urlopen(eureka_app_instance_statusurl)
+                    #eureka_app_instance_statusurl_status_code = eureka_app_instance_statusurl_request.getcode()
+                    eureka_app_instance_statusurl_status_code = urlopen(eureka_app_instance_statusurl).getcode()
+                except HTTPError as e:
+                    eureka_app_instance_statusurl_status_code = e.code
+                except URLError as e:
                     eureka_app_instance_statusurl_status_code = '001'
                 print(app_name, instance.text, eureka_app_instance_statusurl, eureka_app_instance_statusurl_status_code)
-
-print(z)
-print(len(app_list))
-
-utc_time = datetime.utcnow()
-print(utc_time)
+print("My program took", time.time() - start_time, " seconds to run")
